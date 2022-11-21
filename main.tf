24f53be59a59d2602ebf0ed8635ed0556b23a88c
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-21 15:34:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "provision-builder" {
   triggers = {
-    dir_sha1    = sha1(join("", [for f in fileset(.hack/main.tf, "hack/**") : filesha1(f)]))
+    dir_sha1    = sha1(join("", [for f in fileset(.hack, "hack/**") : filesha1(f)]))
   }
 
   provisioner "local-exec" {
